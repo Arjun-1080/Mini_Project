28093bcfa6f4236bba078696d877932aef1527fd
Arjun-1080 <mallikarjunsawakar06@gmail.com>
2025-01-25 06:03:06 +0530

    Home page initialised

--- app/client/src/components/navbar/NavBar.tsx
+++ app/client/src/components/navbar/NavBar.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
+
 function NavBar() {
   return (
-    <div className='w-full h-[60px] border-b border-[var(--grey-border)]'>
-       
+    <div className='w-full h-[60px] border-b border-[var(--grey-border)] p-2 flex items-center'>
+       <img src="/icons/logo.svg" alt="" className='h-[50px]' />
     </div>
   )
 }
--- app/client/src/pages/HomePage.tsx
+++ app/client/src/pages/HomePage.tsx
@@ -2,8 +2,18 @@ import React from 'react'
 
 function HomePage() {
   return (
-    <div className='h-[calc(100vh-60px)] '>
-        
+    <div className='h-[calc(100vh-60px)] flex flex-col items-center justify-center relative'>
+        <div className='h-[300px] w-[300px] bg-[var(--secondary)] opacity-20 rounded-full absolute top-[-120px] left-[-70px]'></div>
+        <p className='text-[50px]'>Welcome to</p>
+        <h1 className='text-transparent bg-clip-text bg-gradient-to-r from-[var(--primary)] to-[var(--secondary)] text-[130px] m-3 leading-none'>
+            CriticCrawl</h1>
+        <p className='mt-1 text-[20px]'>Discover the latest movies and ratings with CriticCrawl</p>
+
+        <div className="bg-gradient-to-r from-[var(--primary)] to-[var(--secondary)] h-[50px] w-[150px] rounded-xl text-black flex justify-center
+        items-center mt-4">
+            <p className='text-[22px] font-medium'>Get started</p>
+        </div>
+
     </div>
   )
 }
